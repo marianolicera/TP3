e06d6961326c90af0d8626d09738f10f08f3d5ba
MarianoLicera <marianolicera3@gmail.com>
2022-05-01 11:02:40 -0300

    Logica de camara finalizada

--- app/src/main/java/com/example/sensores/CameraActivity.java
+++ app/src/main/java/com/example/sensores/CameraActivity.java
@@ -8,11 +8,14 @@ import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionGroupInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,9 +25,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnTomarFoto, btnGuardarFoto;
@@ -64,9 +71,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             checkPermissionCamera();
 
         } else if (id == R.id.btnGuardarFoto) {
-
+            checkPermissionStorage();
         }
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -85,21 +94,35 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 takePicture();
             }
+        }else if (requestCode == REQUEST_PERMISSION_WRITE_STORAGE){
+            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                savePictureGallery();
+                Toast.makeText(this, "Imagen guardada con exito!", Toast.LENGTH_SHORT).show();
+            }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void checkPermissionCamera() { //evaluar permisos y version de sdk
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-                takePicture();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            takePicture();
+        }else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_PERMISSION_CAMERA);
+        }
+
+    }
+
+    private void checkPermissionStorage() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P){
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                savePictureGallery();
             }else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_PERMISSION_CAMERA);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_PERMISSION_WRITE_STORAGE);
             }
 
         }else{
-            takePicture();
+            savePictureGallery();
         }
     }
 
@@ -108,5 +131,70 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         if (intent.resolveActivity(getPackageManager())!=null){
             startActivityForResult(intent, TAKE_PICTURE);
         }
+    }
+
+    private void savePictureGallery(){
+
+        OutputStream outputStream = null;
+        File file = null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){ //API > 29
+            ContentResolver resolver = getContentResolver();
+            ContentValues values = new ContentValues();
+
+            String fileName = System.currentTimeMillis() + "image_example";
+
+            values.put(MediaStore.Images.Media.DISPLAY_NAME,fileName);
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.Images.Media.RELATIVE_PATH,"Pictures/Sensores");
+            values.put(MediaStore.Images.Media.IS_PENDING,1);
+
+            Uri collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+            Uri imageUri = resolver.insert(collection,values);
+
+            try {
+                outputStream = resolver.openOutputStream(imageUri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            values.clear();
+            values.put(MediaStore.Images.Media.IS_PENDING,0);
+            resolver.update(imageUri,values,null,null);
+        }else{ //API < 29
+            String imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+
+            String fileName = System.currentTimeMillis() + ".jpg";
+
+            file = new File(imageDir,fileName);
+
+            try {
+                outputStream = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        boolean saved = bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+        if (saved){
+            Toast.makeText(this,"Imagen guardada satisfactoriamente!",Toast.LENGTH_SHORT).show();
+        }
+
+        if(outputStream != null){ //cerramos el buffer
+            try {
+                outputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (file != null){ //API < 29
+            MediaScannerConnection.scanFile(this,new String[]{file.toString()},null,null);
+        }
+
     }
 }
