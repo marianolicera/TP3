a23085ee6c9e0f1e2b08e8882b1f32f8ca434c20
Pedro Capra <pedrocapra22@hotmail.com>
2022-05-01 12:39:14 -0300

    Agregado vibracion

--- app/src/main/java/com/example/sensores/MainActivity.java
+++ app/src/main/java/com/example/sensores/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnIngresarCamara, btnIngresarFlash;
+    Button btnIngresarCamara, btnIngresarFlash, btnIngresarVibrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnIngresarCamara = (Button) findViewById(R.id.btnCamara);
         btnIngresarFlash = (Button) findViewById(R.id.btnFlash);
+        btnIngresarVibrar = (Button) findViewById(R.id.btnVibrar);
+
 
         btnIngresarCamara.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),FlashActivity.class);
+                startActivity(i);
+            }
+        });
+        btnIngresarVibrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),VibradorActivity.class);
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/com/example/sensores/VibradorActivity.java
@@ -0,0 +1,31 @@
+package com.example.sensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+
+
+public class VibradorActivity extends AppCompatActivity {
+    Button botonvibrador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibrador);
+
+        botonvibrador = findViewById(R.id.btnvibrar);
+
+        botonvibrador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(1000);
+            }
+        });
+    }
+
+}
